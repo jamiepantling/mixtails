abbe7234d45aa114b0b25d12dc80449ab44894b9
rightanglewrong <bennyyang996648@hotmail.com>
2022-06-11 18:02:47 -0400

    Updated Cocktail schema

--- models/Cocktail.js
+++ models/Cocktail.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const IngredientSchema = new Schema({
+  name: {type: String},
+  qty: {type: Number}
+})
+
 const cocktailSchema = new Schema({
   name: String ,
   description: { type: String },
   instruction: { type: String },
-  ingredients: [String],
+  ingredients: [IngredientSchema],
   tags: [String],
+  image: {type: String},
   createdBy: { type: Schema.Types.ObjectId, ref: "User" },
 });
 
