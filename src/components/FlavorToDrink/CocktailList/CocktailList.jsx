2f3dbc5b5068c11bf6716f1340eead454013b9bf
ale lord <j.alexlord@gmail.com>
2022-06-14 14:20:01 -0500

    show cocktail details

--- src/components/FlavorToDrink/CocktailList/CocktailList.jsx
+++ src/components/FlavorToDrink/CocktailList/CocktailList.jsx
@@ -2,8 +2,10 @@ import "./CocktailList.module.css";
 import * as cocktailsAPI from "../../../utilities/cocktails-api";
 import CocktailListItem from "../../FlavorToDrink/CocktailListItem/CocktailListItem";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function CocktailList(props) {
+  const URL = `/cocktails/${props._id}`;
   const [cocktailList, setCocktails] = useState([]);
 
   useEffect(function () {
--- src/components/FlavorToDrink/CocktailListItem/CocktailListItem.jsx
+++ src/components/FlavorToDrink/CocktailListItem/CocktailListItem.jsx
@@ -1,14 +1,24 @@
 import "./CocktailListItem.module.css";
 import * as cocktailsAPI from "../../../utilities/cocktails-api";
+import { useState } from "react";
 
 export default function CocktailListItem(cocktail) {
-  async function deleteCocktail() {
-    await cocktailsAPI.deleteCocktail(cocktail._id);
-  }
-  
-  
-  return <div className="list">
-    {cocktail.name}
-    <button onClick={deleteCocktail}>DELETE</button>
-  </div>;
+  // let showDetails = false;
+  let [showCocktail, setShowCocktail] = useState(false)
+
+  return (
+    <div className="list">
+      {cocktail.name}
+      <button onClick={() => setShowCocktail(!showCocktail)}>Details</button>
+      {showCocktail ? 
+      <div className="details">
+        {/* <p>{cocktail.description}</p> */}
+        <p>{cocktail.instruction}</p>
+        <ul>
+          {cocktail.ingredients.map(ingredient => <li key={ingredient._id}>{ingredient.name}</li>)}
+        </ul>
+      </div> 
+      : <></>}
+    </div>
+  );
 }
