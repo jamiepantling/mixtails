c7270f1f26f79bdf1f134278ed9823d632770bba
Jamie Pantling <99222790+jamiepantling@users.noreply.github.com>
2022-06-15 17:53:45 -0400

    fixed header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,10 +9,9 @@ export default function Header(props) {
         <Link to="/home" className={style.logo} alt="Mixtails Logo"></Link>
         <div className='linksContainer'>
           
-          <Link className={style.navLink} to={"/moods"}>Moods</Link>
-          <Link className={style.navLink} to={"/cocktails"}>Cocktails</Link>
-          <Link className={style.navLink} to={"/flavours"}>Flavas</Link>
-          <Link className={style.navLink} to={"/mixtapes"}>Mixtails</Link>
+          
+          <Link className={style.navLink} to={"/cocktails"}>All cocktails</Link>
+          <Link className={style.navLink} to={"/mixtapes"}>All mixtails</Link>
           <Link className={style.navLink} to={"/user"}>Profile</Link>
         </div>
       </nav>
