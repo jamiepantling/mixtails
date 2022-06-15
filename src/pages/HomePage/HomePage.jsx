9624016b80b98193507b66f2678c1b450d6956f9
ale lord <j.alexlord@gmail.com>
2022-06-15 11:37:50 -0500

    add css styling to home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,17 +1,19 @@
 import Header from "../../components/Header/Header";
 import { Link } from "react-router-dom";
-import style from "./HomePage.module.css"
+import style from "./HomePage.module.css";
 
 export default function HomePage(props) {
   return (
     <main>
       <Header setUserInState={props.setUserInState} />
-      <Link to="/moods" className={style.card1}>
-          WATCHA FEELIN'?
-      </Link>
-      <Link to="/flavours" className={style.card2}>
-          SAVOUR THE FLAVOUR
-      </Link>
+      <div className={style.cardContainer}>
+        <Link to="/moods" className={`${style.card} ${style.one}`}>
+          <span>WATCHA FEELIN'?</span>
+        </Link>
+        <Link to="/flavours" className={`${style.card} ${style.two}`}>
+        <span>SAVOUR THE FLAVOUR</span>
+        </Link>
+      </div>
     </main>
   );
 }
