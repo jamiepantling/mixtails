6e5316944bd00a4ff889914f07c7a435ea4d2214
Jamie Pantling <99222790+jamiepantling@users.noreply.github.com>
2022-09-13 16:13:52 -0400

    style nav bar logo and title

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -44,7 +44,7 @@ export default function Header(props) {
               />
             </Link>
             <Link className={style.titleLinkMobile} to="/home">
-              MixTails
+              mixtails
             </Link>
           </div>
           <div className={style.links}>
