d927cf6d50fbbaa75804b39fe017c4698f2a7adf
Jamie Pantling <99222790+jamiepantling@users.noreply.github.com>
2022-09-14 13:09:40 -0400

    style mood list items

--- src/components/MoodToMix/MoodList/MoodList.jsx
+++ src/components/MoodToMix/MoodList/MoodList.jsx
@@ -1,18 +1,20 @@
 import MoodListItem from "../MoodListItem/MoodListItem";
 import style from "./MoodList.module.css";
 
-
 export default function MoodList(props) {
-
-  const moods = props.moodsList.sort((a,b) => (a.content > b.content) ? 1 : ((b.content > a.content) ? -1 : 0)).map((mood) => (
-    <MoodListItem
-      key={mood._id}
-      {...mood}
-      mixtapesList={props.mixtapesList.filter((mixtape) =>
-        mixtape.moods.find((moods) => moods._id === mood._id)
-      )}
-    />
-  ));
+  const moods = props.moodsList
+    .sort((a, b) =>
+      a.content > b.content ? 1 : b.content > a.content ? -1 : 0
+    )
+    .map((mood) => (
+      <MoodListItem
+        key={mood._id}
+        {...mood}
+        mixtapesList={props.mixtapesList.filter((mixtape) =>
+          mixtape.moods.find((moods) => moods._id === mood._id)
+        )}
+      />
+    ));
 
   return <div className={style.moodsContainer}>{moods}</div>;
 }
